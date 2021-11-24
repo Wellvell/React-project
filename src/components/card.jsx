0fe515496350a478cd67d15daaad086880ca0ea4
Your Name <you@example.com>
2021-11-24 11:19:46 +0300

    initial state update

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -3,6 +3,13 @@ import './assets/styles/card.scss';
 
 function Card(props) {
 
+    const [initialState, setInitialState] = useState({ 
+        word: "server load",
+        transcription: "",
+        translate: "",
+        index: 0,
+        lenght: 0
+    });
     
     const [pressed, setPressed] = useState(false);
 
@@ -10,20 +17,34 @@ function Card(props) {
         setPressed(!pressed);
     }
 
-    
-
-    return (
-        <div className={"card"}>
-            <div className="card__front">
-                <p className="card__front__word">{props.word}</p>
-                <p className="card__front__transcription">{props.transcription}</p>
-                <button onClick={handleChange} className={(pressed ? "card__front__translate" : "card__front__button")}>{
-                    pressed? `${props.translate}`: "Перевод"
-                }</button>
-                <p className="card__front__numbers">{props.index}/{props.length}</p>
-            </div>              
-        </div>
-    );
+    if (props.word === undefined){
+        return (
+            <div className={"card"}>
+                <div className="card__front">
+                    <p className="card__front__word">{initialState.word}</p>
+                    <p className="card__front__transcription">{initialState.transcription}</p>
+                    <button onClick={handleChange} className={(pressed ? "card__front__translate" : "card__front__button")}>{
+                        pressed? `${initialState.translate}`: "Перевод"
+                    }</button>
+                    <p className="card__front__numbers">{initialState.index}/{initialState.length}</p>
+                </div>              
+            </div>
+        );
+    }
+    else{
+        return (
+            <div className={"card"}>
+                <div className="card__front">
+                    <p className="card__front__word">{props.word}</p>
+                    <p className="card__front__transcription">{props.transcription}</p>
+                    <button onClick={handleChange} className={(pressed ? "card__front__translate" : "card__front__button")}>{
+                        pressed? `${props.translate}`: "Перевод"
+                    }</button>
+                    <p className="card__front__numbers">{props.index}/{props.length}</p>
+                </div>              
+            </div>
+        );
+    }
 }
 
 export default Card;
