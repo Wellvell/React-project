ccbc614eb965bb82f2324f6f0febb6aa927b6740
Your Name <you@example.com>
2021-11-25 15:34:45 +0300

    rendering update

--- src/App.js
+++ src/App.js
@@ -3,18 +3,40 @@ import Cards from './components/cards';
 import Footer from './components/footer';
 import Header from './components/header';
 import Table from './components/table';
+import { useState } from 'react';
 
 
 
 function App() {
-  return (
-    <div className="App">
-    <Header></Header>
-    {/*<Table></Table>*/}
-    <Cards></Cards>
-    <Footer></Footer>
-    </div>
-  );
+
+  const [pressedTable, setPressedTable] = useState(false);
+  const handleChangeTable = () => {
+    setPressedTable(true);
+  };
+  const handleChangeCards = () => {
+    setPressedTable(false);
+  };
+
+  if (pressedTable){
+    return (
+      <div className="App">
+      <Header changeModeTable={handleChangeTable} changeModeCards={handleChangeCards}></Header>
+      <Table></Table>
+      <Footer></Footer>
+      </div>
+    );
+  }
+  else{
+    return (
+      <div className="App">
+      <Header changeModeTable={handleChangeTable} changeModeCards={handleChangeCards}></Header>
+      <Cards></Cards>
+      <Footer></Footer>
+      </div>
+    );
+
+  }
+
 }
 
 export default App;
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,15 +2,18 @@ import './assets/styles/header.scss';
 import all from './assets/img/reading-book.png';
 import cards from './assets/img/card-game.png';
 
-function Header() {
+function Header(props) {
+
+    const {changeModeTable, changeModeCards} = props;
+
     return (
         <div className="header">
             <p className="header__text">Welcome to English club!</p>
-            <button className="header__buttons">
+            <button className="header__buttons" onClick={changeModeTable}>
                 <img src={all} alt="btn all words"/>
                 <p className="header__buttons__text">All words</p>
             </button>
-            <button className="header__buttons">
+            <button className="header__buttons" onClick={changeModeCards}>
                 <img src={cards} alt="btn cards"/>
                 <p className="header__buttons__text">My cards</p>
             </button>
