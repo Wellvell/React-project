8c4acee943c2dc4446f16a497c5a6e13b1e6c93a
Lera <khmelenko.lera2010@mail.ru>
2021-12-10 15:02:26 +0300

    focus

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,55 +1,67 @@
-import { useState, useEffect, React} from 'react';
-import './assets/styles/card.scss';
-
+import { useState, useEffect } from "react";
+import "./assets/styles/card.scss";
+import React from "react";
 const Card = React.forwardRef((props, ref) => {
-
-    useEffect(() => ref.current.focus(), []);
-
-    const [initialState, setInitialState] = useState({ 
-        word: "server load",
-        transcription: "",
-        translate: "",
-        index: 0,
-        lenght: 0
-    });
-    
-    const [pressed, setPressed] = useState(false);
-
-    const handleChange = () => {
-        setPressed(!pressed);
-        if (pressed !== true){
-            props.addToCard();
-        }
+  const [initialState, setInitialState] = useState({
+    word: "server load",
+    transcription: "",
+    translate: "",
+    index: 0,
+    lenght: 0,
+  });
+  const [pressed, setPressed] = useState(false);
+  useEffect(() => {
+    ref.current.focus();
+  }, []);
+  const handleChange = () => {
+    setPressed(!pressed);
+    if (pressed !== true) {
+      props.addToCard();
     }
-
-    if (props.word === undefined){
-        return (
-            <div className={"card"}>
-                <div className="card__front">
-                    <p className="card__front__word">{initialState.word}</p>
-                    <p className="card__front__transcription">{initialState.transcription}</p>
-                    <button onClick={handleChange} className={(pressed ? "card__front__translate" : "card__front__button")}>{
-                        pressed? `${initialState.translate}`: "Перевод"
-                    }</button>
-                    <p className="card__front__numbers">{initialState.index}/{initialState.length}</p>
-                </div>              
-            </div>
-        );
-    }
-    else{
-        return (
-            <div className={"card"}>
-                <div className="card__front">
-                    <p className="card__front__word">{props.word}</p>
-                    <p className="card__front__transcription">{props.transcription}</p>
-                    <button ref={ref} onClick={handleChange} className={(pressed ? "card__front__translate" : "card__front__button")}>{
-                        pressed? `${props.translate}`: "Перевод"
-                    }</button>
-                    <p className="card__front__numbers">{props.index}/{props.length}</p>
-                </div>              
-            </div>
-        );
-    }
-})
-
+  };
+  if (props.word === undefined) {
+    return (
+      <div className={"card"}>
+        <div className="card__front">
+          <p className="card__front__word">{initialState.word}</p>
+          <p className="card__front__transcription">
+            {initialState.transcription}
+          </p>
+          <button
+            onClick={handleChange}
+            className={
+              pressed ? "card__front__translate" : "card__front__button"
+            }
+          >
+            {pressed ? `${initialState.translate}` : "Перевод"}
+          </button>
+          <p className="card__front__numbers">
+            {initialState.index}/{initialState.length}
+          </p>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className={"card"}>
+        <div className="card__front">
+          <p className="card__front__word">{props.word}</p>
+          <p className="card__front__transcription">{props.transcription}</p>
+          <button ref={ref}
+            onClick={handleChange}
+            className={
+              pressed ? "card__front__translate" : "card__front__button"
+            }
+          >
+            {pressed ? `${props.translate}` : "Перевод"}
+          </button>
+          <button ref={ref}/> 
+          <p className="card__front__numbers">
+            {props.index}/{props.length}
+          </p>
+        </div>
+      </div>
+    );
+  }
+});
 export default Card;
